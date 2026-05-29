50ccdfdda0203d1c5618d613bb77b66d3b916489
Ajay Kushwaha <129685062+ajaykushwaha0508@users.noreply.github.com>
2026-05-29 12:06:30 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,5 @@
 def call(image ,tag){
+  echo "this build command is form groovy file"
   // sh "docker build -t ${image}:${tag} ." // this is for linux
    bat "docker build -t ${image}:${tag} ." // this is for windows 
 }
